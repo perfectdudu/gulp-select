2f219043796b6e80d2c2d69dc917cb2d8288af1c
perfectdudu <706896418@qq.com>
2016-05-17 17:35:08 +0800

    no message

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 /**
  * Created by xuqing on 16/2/23.
  */
-var gutil = require('gulp-util');
-var through = require('through2');
-var applySourceMap = require('vinyl-sourcemaps-apply');
-var autoprefixer = require('autoprefixer');
+var gutil        = require('gulp-util');
+var PluginError  = gutil.PluginError;
+var through      = require('through2');
+var path         = require('path');
 
+const PLUGIN_NAME = 'gulp-select';
 
-module.exports = function(options){
-    return through.obj(function(file, enc, cb){
-        //主体实现
-    });
-};
+//function selectStream(file){
+//    var stream = through();
+//    stream.write(file);
+//
+//    return stream;
+//}
+
+function select(opts){
+    return through.obj(function(file, enc, callback){
+        if(file.isNull()){
+            this.push(file);
+            return callback();
+        }
+
+        if(file.isStream()){
+            this.emit('error', new PluginError('PLUGIN_NAME', 'stream not supported'));
+            return callback();
+        }
+        //  将满足条件的全部替换!
+        var content = file.contents.toString('utf-8');
+
+        content = content.replace(opts.reg, opts.repstring);
+        //this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'uglify inline scripts error: ' ));
+        file.contents = new Buffer(content);
+        this.push(file);
+        callback();
+    })
+}
+
+module.exports = select;
